9c0b7ee5089dd5786c783a21aee5ca1f9d6adcd1
Swannhs <princeapurbask@gmail.com>
2021-05-12 01:15:52 +0600

    Get & display data through redux

--- src/view/Products/Products.js
+++ src/view/Products/Products.js
@@ -1,21 +1,29 @@
-import React, {Component} from 'react';
-import axios from "axios";
+import React, {useState, useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {listProducts} from "../../components/actions/productActions";
+import {Col, Row} from "react-bootstrap";
+import Product from "./Product";
 
-class Products extends Component {
-    componentDidMount() {
-        axios.get('http://localhost:8000/')
-            .then(response => {
-                console.log(response)
-            })
-    }
+function Products() {
+    const dispatch = useDispatch();
+    const productList = useSelector(state => state.productList)
+    const {error, loading, products} = productList
 
-    render() {
-        return (
-            <div>
+    useEffect(() => {
+        dispatch(listProducts())
+    }, [])
 
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Row>
+                {products.map(product => (
+                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                        <Product product={product}/>
+                    </Col>
+                ))}
+            </Row>
+        </div>
+    )
 }
 
 export default Products;
